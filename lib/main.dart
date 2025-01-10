ad4b0d49b87af723160d84fbb60a35de02aabc96
Thilrash Gowzul Ameen <36620465+Thilrash@users.noreply.github.com>
2025-01-10 22:34:48 +0530

    Initial File Push

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipes Book',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+            titleTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
+      ),
+      home: ,
+    );
+  }
+}
